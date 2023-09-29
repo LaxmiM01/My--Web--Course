2b38c732212bc7fe0a1f204d976495ac0c9802ac
LaxmiM01 <manpatti.laxmi@gmail.com>
2023-09-29 23:07:05 +0530

    JS ifelse.js

---
+++ JavaScript/ifelse.js
@@ -0,0 +1,12 @@
+//If else condition:
+ //there are three types of conditions. 
+ //1.if condition
+ //2.ifelse condition
+ //3.ifelse ifelse condition
+
+
+ let string = prompt("enter a name: ")
+ if(string = true){
+     console.log("The name is printed")
+ }
+ console.log("the name is not printed")
