a742402c29f7397d83edc3cc23d161103565f4cb
LaxmiM01 <manpatti.laxmi@gmail.com>
2023-09-30 16:49:56 +0530

    JS ifelse.js

--- JavaScript/ifelse.js
+++ JavaScript/ifelse.js
@@ -5,8 +5,90 @@
  //3.ifelse ifelse condition
 
 
- let string = prompt("enter a name: ")
- if(string = true){
-     console.log("The name is printed")
- }
- console.log("the name is not printed")
+//  let string = prompt("enter a name: ")
+//  if(string = true){
+//      console.log("The name is printed")
+//  }
+//  console.log("the name is not printed")
+
+
+// if else condition
+
+// let greet = prompt("enter a time: ")
+// if(greet = true){
+//     console.log("good morning")
+    
+// }if(greet = false){
+//     console.log("good after noon")
+// }else{
+//     console.log("good evening")
+// }
+
+// JavaScript if...else if statement:
+
+// The if...else statement is used to execute a block of code among two alternatives. However, if you need to make a choice between more than two alternatives, if...else if...else can be used.
+
+// The syntax of the if...else if...else statement is:
+// if (condition1) {
+//     // code block 1
+// } else if (condition2){
+//     // code block 2
+// } else {
+//     // code block 3
+// }
+
+
+
+
+
+//Leap year program:
+//Write a function isLeapYear that takes a year as input and prints "Leap year" if the year is
+// divisible by 4 and not divisible by 100, or if it's divisible by 400. Otherwise, print "Not a leap
+// year."
+
+
+const year = prompt('Enter a year: ')
+  checkLeapYear(year);
+
+function checkLeapYear(year){
+    
+    
+    if((0 == year % 4) && (0 != year % 100)||(0 == year % 400)){
+        
+       console.log(year + ' is a leap year');
+    }
+    else{
+        console.log(year + ' it is not leap year ')
+    }
+    
+}
+
+
+
+
+
+
+
+
+// how to work:
+// If condition1 evaluates to true, the code block 1 is executed.
+// If condition1 evaluates to false, then condition2 is evaluated.
+// If the condition2 is true, the code block 2 is executed.
+// If the condition2 is false, the code block 3 is executed.
+
+
+
+let num = prompt("enter the number: ")
+if(num>10){
+    console.log("the number is retun")
+}
+else if(num<10){
+    console.log("the number is equal")
+}
+else{
+    console.log("the number is -ve")
+}
+console.log("complete output")
+
+
+
