b0dec9f19a7e02c85139180964e390f7effb3a0f
LaxmiM01 <manpatti.laxmi@gmail.com>
2024-03-20 12:38:18 +0530

    Add forloop

---
+++ JavaScript/forloop/sum.js
@@ -0,0 +1,66 @@
+//Sum of Natural numbers
+//natural Number = 1 to Infinity
+
+// let sum = 0
+// const n = 10
+// for( let i = 0; i<=n; i++){
+//     sum += i;
+// }
+// console.log(sum)
+
+//factorial of a number
+//n!
+
+// const n = 5
+// let factorial = 1
+// for( let i = 1; i<= n; i++){
+//     factorial*= i;
+
+// }
+// console.log(factorial)
+
+//OR
+
+// function factorial (n) {
+//     let result = 1;
+//     for(i= 1; i<= n; i++){
+//         result*= i
+//     }
+//     return result;
+// }
+// console.log(factorial(6))
+
+//Fibonacci Sequence
+
+// const n = 7
+// let first = 0;
+// let second = 1, next;
+// // console.log(first)
+// // console.log(second)
+// for( let i = 2; i<n; i++) {
+//     next = first + second
+//     console.log(next)
+//     first = second
+//     second = next;
+// }
+
+//OR anther method
+
+// function fibonacci(n) {
+//     let fibsequence = [0,1]
+//     for( let i = 2; i<=n; i++){
+//         fibsequence.push(fibsequence[i-1] + fibsequence[i-2])
+//     }
+//     return fibsequence.slice(0,n)
+// }
+// console.log(fibonacci(10))
+
+//Mulitiplication Table
+
+// const n = 10
+// for( let i = 1; i<=n; i++){
+//     let result = i*5;
+//     console.log(5 + "*" + i + "=" + result)
+// }
+
+
